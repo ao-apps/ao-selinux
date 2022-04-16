f318197156e1cc569b0e35eab4806a0d78545966
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:30 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/selinux/SEManagePort.java
+++ src/main/java/com/aoindustries/selinux/SEManagePort.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-selinux.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.selinux;
 
 import com.aoapps.collections.AoCollections;
